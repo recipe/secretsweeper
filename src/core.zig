0579fd44ea9939b3a99e37e60c419ab3ec26f323
Vitaliy Demidov <vital.letter@gmail.com>
2025-09-11 19:03:01 -0700

    Tests passed.

--- src/core.zig
+++ src/core.zig
@@ -1,9 +1,103 @@
 const py = @import("pydust");
-
+const std = @import("std");
+const ffi = py.ffi;
 const root = @This();
 
-pub fn mask() !py.PyString(root) {
-    return try py.PyString(root).create("Hello, Sweeper!");
+const allocator = std.heap.page_allocator;
+
+pub const NoNewLineIterator = struct {
+    data: [:0]const u8,
+    index: usize = 0,
+
+    /// Initializes an iterator
+    pub fn create(data: [:0]const u8) NoNewLineIterator {
+        return .{ .data = data};
+    }
+
+    /// Returns the next no new line character with its index: (absolute_index, character)
+    pub fn next(self: *NoNewLineIterator) ?struct {usize, u8} {
+        while (self.index < self.data.len) {
+            const pos = self.index;
+            const char = self.data[self.index];
+            self.index += 1;
+            if (char != '\n') {
+                return .{pos, char};
+            }
+        }
+        return null;
+    }
+
+    pub fn reset(self: *NoNewLineIterator) void {
+        self.index = 0;
+    }
+};
+
+
+pub fn mask(args: struct {input: py.PyBytes(root), words: py.PyTuple(root)}) !py.PyBytes(root) {
+    const input_slice = try args.input.asSlice();
+    var input_it = NoNewLineIterator.create(input_slice);
+
+    var buf = try allocator.allocSentinel(u8, input_slice.len, 0);
+    defer allocator.free(buf);
+    @memcpy(buf, input_slice);
+
+    for (0..args.words.length()) |pos| {
+        const item = try args.words.getItem(py.PyBytes(root), pos);
+        var item_it = NoNewLineIterator.create(try item.asSlice());
+
+        var item_i: usize = undefined;
+        var item_char: u8 = undefined;
+        if (item_it.next()) |p| {
+            item_i, item_char = p;
+        } else {
+            continue;
+        }
+        const item_start = item_i;
+        const item_first_char = item_char;
+
+        var match_start: usize = 0;
+        var return_pos: usize = 0;
+        while (input_it.next()) |r| {
+            const i, const char = r;
+            // std.debug.print("{c} . {c}\n", .{ char, item_char });
+            if (char == item_char) {
+                // std.debug.print("{c}={c}\n", .{ char, item_char });
+                if (item_i == item_start) {
+                    match_start = i;
+                } else if (return_pos == 0 and char == item_first_char) {
+                    return_pos = i;
+                }
+                if (item_it.next()) |p| {
+                    item_i, item_char = p;
+                } else {
+                    for (buf[match_start..i + 1]) |*b| {
+                        if (b.* != '\n') b.* = '*';
+                    }
+                    item_it.reset();
+                    if (item_it.next()) |p| {
+                        item_i, item_char = p;
+                    }
+                    return_pos = 0;
+                }
+            } else if (item_i > item_start) {
+                // std.debug.print("{c} <> {c}\n", .{ char, item_char });
+                item_it.reset();
+                if (item_it.next()) |p| {
+                    item_i, item_char = p;
+                }
+                if (return_pos == 0) {
+                    input_it.index -= 1;
+                } else {
+                    input_it.index = return_pos;
+                    return_pos = 0;
+                }
+
+            }
+        }
+        input_it.reset();
+    }
+    const res = py.PyBytes(root).create(buf);
+    return res;
 }
 
 comptime {
--- test/test_secretsweeper.py
+++ test/test_secretsweeper.py
@@ -1,5 +1,32 @@
+import pytest
+
 import secretsweeper
 
-
-def test_mask():
-    assert secretsweeper.mask() == 'Hello, Sweeper!'
+@pytest.mark.parametrize(
+    ("data", "values", "expected"),
+    [
+        ("teststring", ["string"], "test******"),
+        ("testingstring", ["string", "testing"], "*************"),
+        ("testing(val)string", ["string", "testing"], "*******(val)******"),
+        ("1ttestingsstringg", ["string", "testing"], "1t*******s******g"),
+        ("string and other things", ["string"], "****** and other things"),
+        ("1ttes\ntingss\ntringg", ["string", "testing"], "1t***\n****s*\n*****g"),
+        ("testing\nstring\n", ["string", "testing"], "*******\n******\n"),
+        ("testing\n", ["testing"], "*******\n"),
+        ("testingothertesting\n", ["testing"], "*******other*******\n"),
+        # overlapping values
+        ("12foobarbaz", ["12foobarbaz", "other121"], "***********"),
+        ("qquerty", ["querty"], "q******"),
+        ("cbcbccb", ["cbccb"], "cb*****"),
+        ("bcbcbccb", ["cbccb", "bcbcb"], "*" * len("bcbcbccb")),
+        ("4RIPUG5YE", ["RIPUG", "4I3CT"], "4*****5YE"),
+        ("TXXXXICHTAX", ["XXICH"], "TXX*****TAX"),
+        ("TX\n\n\nX\n\nX\nX\n\nICHTAX", ["XXICH"], "TX\n\n\nX\n\n*\n*\n\n***TAX"),
+        ("TX\n\n\nX\n\nX\nX\n\nICHTAX", ["X\nX\nICH"], "TX\n\n\nX\n\n*\n*\n\n***TAX"),
+        ("S7SUUUUZPE", ["S7SUU", "UUZPE"], "**********"),
+        ("all-removed\nnot-removed\n", ["removed", "all-removed"], "***********\nnot-*******\n"),
+        # ("terraform\n", ["terra\nform"], "terraform\n"),  # reserved word must remain intact
+    ],
+)
+def test_mask(data: str, values: list[str], expected: str) -> None:
+    assert secretsweeper.mask(data.encode(), tuple(v.encode() for v in values)) == expected.encode()
