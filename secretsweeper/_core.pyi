09b10b86b377d57bf15d81a1405d25dec9cd4ed0
Vitaliy Demidov <vital.letter@gmail.com>
2025-09-12 21:18:09 -0700

    Fixed a typo.

--- secretsweeper/_core.pyi
+++ secretsweeper/_core.pyi
@@ -2,9 +2,9 @@ def mask(input: bytes, words: tuple[bytes, ...], /, *, limit: int = 15) -> bytes
     """
     Masks the specific words in the input.
 
-    :param input: An input bytes.
+    :param input: An input.
     :param words: A tuple of secrets that have to be masked with the `*` asterisk character.
-    :param limit: A max number of consecutive asterisks.
+    :param limit: The max number of consecutive stars.
     :return Rreturns the input string with masked secrets.
     """
     ...
