2af8ee853bd19f41fa422f50b8b227b5a50ac496
Vitaliy Demidov <vital.letter@gmail.com>
2026-01-11 20:02:04 -0800

    wip

--- pre-build.sh
+++ pre-build.sh
@@ -5,14 +5,14 @@ set -e
 echo "Installing poetry..."
 pip install poetry
 
+echo "Installing dependencies..."
+poetry install
+
+pip freze
+
 echo "Creating a symlink to the pydust library..."
 PYDUST_PATH=`python -c 'import sysconfig; print(sysconfig.get_paths()["purelib"])'`/pydust
 ls -al $PYDUST_PATH
 
 ln -sf $PYDUST_PATH pydust
 ls -al pydust
-
-echo "Installing dependencies..."
-poetry install
-
-pip freze
