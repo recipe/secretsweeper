e0e0eb5dada6b0a1b6a5bc1dd77ef0166c0c4d49
Vitaliy Demidov <vital.letter@gmail.com>
2026-01-12 17:33:50 -0800

    wip

--- pre-build.sh
+++ pre-build.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-
 echo "Installing uv..."
 curl -LsSf https://astral.sh/uv/0.8.22/install.sh | sh
 source $HOME/.local/bin/env
@@ -17,19 +16,4 @@ ln -sf $PYDUST_PATH ./pydust
 ls -al ./pydust
 
 echo "Building the package"
-poetry build
-
-#echo "Installing poetry..."
-#pip install poetry
-#
-#echo "Installing dependencies..."
-#poetry install --no-root
-#
-#pip freeze
-#
-#echo "Creating a symlink to the pydust library..."
-#PYDUST_PATH=`python -c 'import sysconfig; print(sysconfig.get_paths()["purelib"])'`/pydust
-#ls -al $PYDUST_PATH
-#
-#ln -sf $PYDUST_PATH pydust
-#ls -al pydust
+pytest test -v
