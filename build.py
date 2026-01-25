042cd1635461af4c95d0c73ad0993f2f33de2e53
Vitaliy Demidov <vital.letter@gmail.com>
2026-01-25 13:38:55 -0800

    cibuildwheel based build

--- build.py
+++ build.py
@@ -1,15 +1,22 @@
+import contextlib
 import shutil
+import typing
 from pathlib import Path
 
+import pydust
 from pydust.build import build
 
-import pydust
 
-dst = Path("pydust/src/ffi.h")
-dst.parent.mkdir(parents=True, exist_ok=True)
-shutil.copyfile(
-    Path(pydust.__file__).parent / "src" / "ffi.h",
-    dst
-)
+@contextlib.contextmanager
+def ensure_ffi_location() -> typing.Iterator[None]:
+    """For some reason, `zig translate-c` is looking for `ffi.h` in the wrong location."""
+    dst = Path("pydust/src/ffi.h")
+    dst.parent.mkdir(parents=True, exist_ok=True)
+    src = Path(pydust.__file__).parent / "src" / "ffi.h"
+    shutil.copyfile(src, dst)
+    yield
+    shutil.rmtree(Path("pydust"))
 
-build()
+
+with ensure_ffi_location():
+    build()
