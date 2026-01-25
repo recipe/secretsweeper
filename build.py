87922bf3e17a92504552669bc0fbc5103a62d37e
Vitaliy Demidov <vital.letter@gmail.com>
2026-01-24 21:28:55 -0800

    cibuildwheel based build

--- build.py
+++ build.py
@@ -1,3 +1,15 @@
+import shutil
+from pathlib import Path
+
 from pydust.build import build
+
+import pydust
+
+dst = Path("pydust/src/ffi.h")
+dst.parent.mkdir(parents=True, exist_ok=True)
+shutil.copyfile(
+    Path(pydust.__file__).parent / "src" / "ffi.h",
+    dst
+)
 
 build()
