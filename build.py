83a67a528958d2bc4790e516a43773b74014b9f7
Vitaliy Demidov <vital.letter@gmail.com>
2026-02-01 16:52:41 -0800

    Windows build (2)

--- build.py
+++ build.py
@@ -1,5 +1,7 @@
 import contextlib
 import shutil
+import sys
+import sysconfig
 import typing
 from pathlib import Path
 
@@ -7,9 +9,23 @@ import pydust
 from pydust.build import build
 
 
+def _ensure_ld_library() -> None:
+    """
+    Pydust assumes LDLIBRARY is always set, but on Windows (especially in PEP 517 isolated builds) it may be None.
+    Provide a correct fallback.
+    """
+    if sys.platform != "win32":
+        return
+
+    if sysconfig.get_config_var("LDLIBRARY") is None:
+        sysconfig._CONFIG_VARS["LDLIBRARY"] = f"python{sys.version_info.major}{sys.version_info.minor}.lib"
+
+
 @contextlib.contextmanager
 def ensure_ffi_location() -> typing.Iterator[None]:
-    """For some reason, `zig translate-c` is looking for `ffi.h` in the wrong location."""
+    """
+    For some reason, `zig translate-c` is looking for `ffi.h` in the wrong location.
+    """
     dst = Path("pydust/src/ffi.h")
     dst.parent.mkdir(parents=True, exist_ok=True)
     src = Path(pydust.__file__).parent / "src" / "ffi.h"
@@ -17,6 +33,8 @@ def ensure_ffi_location() -> typing.Iterator[None]:
     yield
     shutil.rmtree(Path("pydust"))
 
+
+_ensure_ld_library()
 
 with ensure_ffi_location():
     build()
